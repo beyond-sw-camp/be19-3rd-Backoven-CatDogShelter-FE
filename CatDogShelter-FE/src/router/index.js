380ffdd933485973edb6be5556e499fb8b4e92fb
haenin <gpdnjsqhdld@naver.com>
2025-10-27 14:55:17 +0900

    입양 게시판 구현중

--- CatDogShelter-FE/src/router/index.js
+++ CatDogShelter-FE/src/router/index.js
@@ -2,7 +2,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import HomeView from '@/views/HomeView.vue'
-import VolunteerView from '@/views/VolunteerView.vue'
+import VolunteerView from '@/views/volunteer/VolunteerView.vue'
 import DonationView from '@/views/DonationView.vue'
 import AdoptionView from '../views/adoption/AdoptionView.vue'
 import MissingView from '@/views/MissingView.vue'
@@ -18,13 +18,40 @@ import AdoptionProcessView from '@/views/footer/AdoptionProcessView.vue'
 import VolunteerGuideView from '@/views/footer/VolunteerGuideView.vue'
 import PostWriteView from '@/views/Post/PostWriteView.vue'
 import FaqView from '@/views/footer/FaqView.vue'
+
+import ShelterheadMypageView from '@/views/ShelterheadMypageView.vue'
+import VolunteerReviewDetailView from '@/views/volunteer/VolunteerReviewDetailView.vue'
+import VolunteerReviewInsertView from '@/views/volunteer/VolunteerReviewInsertView.vue'
+
 import LoginPlaceholderView from '@/views/LoginPlaceholderView.vue'
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', name: 'home', component: HomeView },
-        { path: '/volunteer', name: 'volunteer', component: VolunteerView },
+
+        // 봉사게시판 라우팅
+        {
+          path: '/volunteer',
+          component: VolunteerView, // 봉사 게시판 메인
+          children: [
+                      {
+                        path: 'detail/:id', // 봉사모집 상세 라우팅
+                        name: 'VolunteerDetail',
+                        component: () => import('@/views/volunteer/VolunteerDetailView.vue')
+                      },
+                      {
+                        path: 'review/:id', // 봉사후기 상세 라우팅
+                        name: 'VolunteerReviewDetail',
+                        component: () => import('@/views/volunteer/VolunteerReviewDetailView.vue')
+                      },
+                      {
+                        path: 'review/insert', // 봉사후기 상세 라우팅
+                        name: 'VolunteerReviewInsert',
+                        component: () => import('@/views/volunteer/VolunteerReviewInsertView.vue')
+                      }
+                    ]
+        },
         { path: '/donation', name: 'donation', component: DonationView },
     { 
         path: '/adoption', 
@@ -60,6 +87,17 @@ const router = createRouter({
         { path: '/adoption-process', component: AdoptionProcessView },
         { path: '/volunteer-guide', component: VolunteerGuideView },
         { path: '/faq', component: FaqView },
+        { 
+          path: '/shelter-head/mypage',                 // 보호소장 마이페이지 라우팅
+          component: ShelterheadMypageView,
+          children: [
+                      { 
+                        path: 'recruitinsert',          // 봉사모집 게시글 작성 라우팅
+                        name: 'VolunteerRecruitInsert',
+                        component: () => import('@/views/volunteer/VolunteerRecruitInsertView.vue')
+                      }
+                    ]
+        },
         { path: '/login', name: 'login', component: LoginPlaceholderView }
     ],
     
